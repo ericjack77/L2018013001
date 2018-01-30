719473c23527163430b968c7e5e26a9aa00633ff
ericchiu <ericjack7@gmail.com>
2018-01-30 14:37:15 +0800

    利用android內建方法 Geocoder 座標轉地名

--- app/src/main/java/com/example/student/l2018013001/MainActivity.java
+++ app/src/main/java/com/example/student/l2018013001/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.student.l2018013001;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,6 +14,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import java.io.IOException;
+import java.util.List;
 
 import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
@@ -71,34 +77,46 @@ public class MainActivity extends AppCompatActivity {
         }
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new MyListener());
     }
+
+    class MyListener implements LocationListener{
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("LOC","Change!!");
+            Log.d("LOC","Change!!"+location.getLatitude()+","+location.getLongitude());
+            Location loc101 = new Location("LOC");
+            loc101.setLatitude(25.0336); //緯度
+            loc101.setLongitude(121.5646); //徑度
+
+            float dis = location.distanceTo(loc101);
+            Log.d("LOC", "Dist:" + dis);
+
+            Geocoder coder =new Geocoder(MainActivity.this);
+
+            try {
+                List<Address> mylist = coder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                Address addr = mylist.get(0);
+                Log.d("LOC",addr.getAddressLine(0));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    }
 }
 
-class MyListener implements LocationListener{
 
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d("LOC","Change!!");
-        Log.d("LOC","Change!!"+location.getLatitude()+","+location.getLongitude());
-        Location loc101 = new Location("LOC");
-        loc101.setLatitude(25.0336); //緯度
-        loc101.setLongitude(121.5646); //徑度
-
-        float dis = location.distanceTo(loc101);
-        Log.d("LOC", "Dist:" + dis);
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-}
